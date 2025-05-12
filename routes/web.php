81b78d3a4aee6d862e9c112c225484b09a7a9344
Geek1498 <simbamasar4@gmail.com>
2025-05-12 09:08:17 +0200

    Add routes for bond summary and admin bond summaries views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\BondSummary;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/summary', function () {
+    $latest = BondSummary::latest('week_end')->first();
+
+    return view('summary', [
+        'summary' => $latest?->summary ?? 'No summary available.',
+        'yieldData' => $latest?->raw_yield_data ?? [],
+        'dates' => $latest ? [
+            'start' => $latest->week_start->toFormattedDateString(),
+            'end' => $latest->week_end->toFormattedDateString(),
+        ] : null,
+    ]);
+});
+
+Route::get('/admin/bond-summaries', function () {
+    $summaries = BondSummary::orderBy('week_end', 'desc')->get();
+
+    return view('admin.bond_summaries', compact('summaries'));
 });
